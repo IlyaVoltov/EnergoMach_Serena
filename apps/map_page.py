53ae4925f0cbe4806371b9d9c0e0ef55a3193321
voltov <ivoltovp@gmail.com>
2021-05-22 22:30:26 +0300

    last_version_FE

--- apps/map_page.py
+++ apps/map_page.py
@@ -24,92 +24,18 @@ region_name = df.region
 city_name = df.city
 wind_name = df.wind
 rain_name = df.rain
+temperature_name = df.temperature
 date_name = df.date
 probability_name = df.probability
-
 
 """ LAYOUT """
 mapbox_access_token = open("data//token.mapbox_token").read()
 
-# fig = go.Figure()
-# fig.update_layout(
-#             legend=dict(
-#                 x=0,
-#                 y=1,
-#                 title_text='Легенда карты (нажмите)',
-#                 title_font_family="Rockwell",
-#                 font=dict(
-#                     family="Rockwell",
-#                     size=12,
-#                     color="black"
-#                 ),
-#                 bgcolor="LightCyan",
-#                 bordercolor="Cyan",
-#                 borderwidth=2
-#             ),
-#             title='Интерактивная карта аварийных отключений',
-#             font=dict(
-#                 family="Rockwell",
-#                 size=17,
-#                 color="rgb(47, 79, 79)"
-#             ),
-#             autosize=True,
-#             hovermode="closest",
-#             hoverlabel=dict(
-#                 bgcolor="white",
-#                 font_size=14,
-#                 font_family="Rockwell"
-#             ),
-#             showlegend=True,
-#             mapbox=dict(
-#                 accesstoken=mapbox_access_token,
-#                 bearing=0,
-#                 center=dict(
-#                     lat=56.25,
-#                     lon=40.15
-#                 ),
-#                 pitch=0,
-#                 zoom=5,
-#                 style='satellite'
-#             ),
-#         )
-# fig.add_trace(go.Scattermapbox(
-#             name='Центры регионов',
-#             lat=site_lat,
-#             lon=site_lon,
-#             mode='markers',
-#             marker=go.scattermapbox.Marker(
-#                 size=21,
-#                 opacity=0.8,
-#                 color='rgb(0, 255, 44)',
-#                 ),
-#             text=region_name,
-#             hoverinfo='text'
-#         ))
-# '''HARDCODE'''
-# fig.add_trace(go.Scattermapbox(
-#             name='Регионы с вероятностью аварии',
-#             lat=['56.1281561', '48.7072005', '59.2484186', '51.6592378'],
-#             lon=['40.4082995', '44.5170207', '39.8356461', '39.1968284'],
-#             mode='markers',
-#             marker=go.scattermapbox.Marker(
-#                 size=12,
-#                 opacity = 0.8,
-#                 color = 'rgb(254, 1, 1)',
-#                 symbol = 'circle'
-#             ),
-#             text=["Ветер и гроза", "Ветер и гроза", "Ветер и гроза",
-#                 "Ветер и гроза"],
-#             hoverinfo='text'
-#         ))
-
 column_names = [{"name": "№", "id": "number"},
-                #{"name": "Широта", "id": "lat"},
-                #{"name": "Долгота", "id": "lon"},
                 {"name": "Регион", "id": "region"},
                 {"name": "Центр региона", "id": "city"},
                 {"name": "Ветер, м/с", "id": "wind"},
-                {"name": "Дождь", "id": "rain"},
+                {"name": "Метео условия", "id": "rain"},
                 {"name": "Температура", "id": "temperature"},
                 {"name": "Прогноз на", "id": "date"},
                 {"name": "Процент вероятности аварии", "id": "probability"},
@@ -117,11 +43,11 @@ column_names = [{"name": "№", "id": "number"},
 
 date_selector = dcc.RangeSlider(
     id='slider',
-    min=min(df.probability),
-    max=max(df.probability),
-    marks={0: '0', 10: '10', 20: '20', 30: '30', 40: '40', 50: '50', 60: '60', 70: '70', 80: '80', 90: '90', 100: '100'},
+    min=0,
+    max=50,
+    marks={0: '0', 10: '10', 20: '20', 30: '30', 40: '40', 50: '50'},
     step=1,
-    value=[25, 75]
+    value=[0, 50]
 )
 
 app = dash.Dash(__name__)
@@ -140,7 +66,6 @@ app.layout = html.Div([
         'background-color': "rgb(176, 224, 230)"}
         ),
     dcc.Graph(
-    #figure=fig,
     id='map',
     style={
         'color': "rgb(47, 79, 79)",
@@ -164,7 +89,6 @@ app.layout = html.Div([
     dash_table.DataTable(
         id='table',
         columns=column_names,
-        #title='Интерактивная карта аварийных отключений',
         style_data_conditional=[{
             'if': {'column_editable': False},
             'textAlign': 'center',
@@ -232,7 +156,7 @@ def update_map(update):
                     lon=40.15
                 ),
                 pitch=0,
-                zoom=5,
+                zoom=4,
                 style='satellite'
             ),
         )
@@ -242,27 +166,54 @@ def update_map(update):
                 lon=new_data.lon,
                 mode='markers',
                 marker=go.scattermapbox.Marker(
-                    size=21,
-                    opacity=0.8,
+                    size=28,
+                    opacity=0.9,
                     color='rgb(0, 255, 44)',
                     ),
-                text=region_name,
+                text=new_data.region,
                 hoverinfo='text'
             ))
     '''HARDCODE'''
     fig.add_trace(go.Scattermapbox(
-            name='Регионы с вероятностью аварии',
-            lat=['56.1281561', '48.7072005', '59.2484186', '51.6592378'],
-            lon=['40.4082995', '44.5170207', '39.8356461', '39.1968284'],
+            name='Регионы с плохими погодными условиями - град',
+            lat=['62.0280273'],
+            lon=['129.7325717'],
             mode='markers',
             marker=go.scattermapbox.Marker(
-                size=12,
+                size=11,
                 opacity = 0.8,
                 color = 'rgb(254, 1, 1)',
                 symbol = 'circle'
             ),
-            text=["Ветер и гроза", "Ветер и гроза", "Ветер и гроза",
-                "Ветер и гроза"],
+            text=["Сильный град"],
+            hoverinfo='text'
+        ))
+    fig.add_trace(go.Scattermapbox(
+            name='Регионы с плохими погодными условиями - дождь',
+            lat=['57.2050177', '56.4847036', '54.5060439', '57.8029445', '54.5060439', '59.2484186', '56.1281561'],
+            lon=['39.4378357', '84.9481737', '36.2515933', '40.9907282', '36.2515933', '39.8356461', '40.4082995'],
+            mode='markers',
+            marker=go.scattermapbox.Marker(
+                size=11,
+                opacity = 0.8,
+                color = 'rgb(0, 54, 255)',
+                symbol = 'circle'
+            ),
+            text=["Сильный дождь", "Сильный дождь", "Сильный дождь", "Сильный дождь", "Дождь", "Дождь", "Дождь"],
+            hoverinfo='text'
+        ))
+    fig.add_trace(go.Scattermapbox(
+            name='Регионы с плохими погодными условиями - ветер более 15 м/с',
+            lat=['54.734853'],
+            lon=['55.9578647'],
+            mode='markers',
+            marker=go.scattermapbox.Marker(
+                size=11,
+                opacity = 0.8,
+                color = 'rgb(2, 229, 254)',
+                symbol = 'circle'
+            ),
+            text=["Сильный северный ветер"],
             hoverinfo='text'
         ))
     
